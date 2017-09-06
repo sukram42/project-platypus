a9adf2ad97b24791f35459b35b99a1a325325ea5
Markus <markus.boebel1@gmail.com>
2017-09-06 15:45:43 +0100

    Fixed Flux Infrastructure

--- webapp/src/js/actions/DataActions.js
+++ webapp/src/js/actions/DataActions.js
@@ -5,12 +5,14 @@
 import dispatcher from '../dispatchers/dispatcher';
 import axios from 'axios';
 
-export function fetchCompanyInformation(symbol) {
+export const POLLING_TIME= 10000;
+
+export function fetchCompanyData(symbol) {
   console.log("Action aufgerufen");
   axios.get('https://api.iextrading.com/1.0/stock/' + symbol + '/company')
     .then(response=>{
       dispatcher.dispatch({
-        "type":'FETCH_COMPANY_INFO',
+        "type":'FETCH_COMPANY_ONE',
         "data": response.data
       })
     });
@@ -19,11 +21,42 @@ export function fetchCompanyInformation(symbol) {
 export function fetchData()
 {
   //TODO Port ändern
-  axios.get('localhost:3001/api/companies')
+  axios.get('http://localhost:3001/api/companies')
     .then(response=>{
       dispatcher.dispatch({
         "type":'FETCH_COMPANY_DATA',
         "data": response.data
       })
     });
+}
+
+export function startPolling()
+{
+  if(!this.interval) {
+    this.interval = setInterval(() => {
+      fetchData();
+    }, POLLING_TIME);
+  }
+}
+export function stopPolling()
+{
+  if(this.interval){
+    clearInterval(this.interval);
+  }
+}
+
+export function startPollingCompany(symbol)
+{
+  console.log("SYMBOL",symbol);
+  if(!this.interval) {
+    this.companyInterval[symbol] = setInterval(() => {
+      fetchCompanyData();
+    }, POLLING_TIME);
+  }
+}
+export function stopPollingCompany(symbol)
+{
+  if(this.companyInterval[symbol]){
+    clearInterval(this.interval);
+  }
 }
