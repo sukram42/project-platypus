aeb2dfa7d677b9155207a58d8e72b8a9e04110ad
Markus <markus.boebel1@gmail.com>
2017-09-06 15:45:25 +0100

    Fix exchange opening hours

--- datamining/data.js
+++ datamining/data.js
@@ -13,15 +13,15 @@ var INITIALIZE_DB = true;
  * HPE   | Hewlett Packard Enterprise
  * IBM   | IBM
  * DXC   | DXC Technology
- * DVMT  | DELL 
+ * DVMT  | DELL
  * CSCO  | Cisco
  * INTC  | Intel
  * SMCI  | Super Micro
  * GOOGL | Google
- * 
+ *
  * @type {[*]}
  */
-var symbols = ["AAPL","HPE","IBM","DXC","DVMT","CSCO","INTC",];
+var symbols = ["AAPL", "HPE", "IBM", "DXC", "DVMT", "CSCO", "INTC","SAP","ORCL"];
 
 var requestify = require('requestify');
 var mongo = require('mongodb').MongoClient;
@@ -30,16 +30,16 @@ var moment = require('moment');
 
 /**Standard URL to get the information
  *
-%#company#% -> Placeholder for the company symbol*/
-var standardURL =  "https://api.iextrading.com/1.0/stock/%#company#%/quote?filter=symbol,latestTime,latestPrice,latestVolume,change,delayedPrice,delayedPriceTime";
+ %#company#% -> Placeholder for the company symbol*/
+var standardURL = "https://api.iextrading.com/1.0/stock/%#company#%/quote?filter=symbol,latestTime,latestPrice,latestVolume,change,delayedPrice,delayedPriceTime";
 
 var iteration = 0;
 
 /**
  * Start Initialization if its wanted
  */
-INITIALIZE_DB?initDB():console.log("No Initalization");
-setInterval(getData,INTERVAL);
+INITIALIZE_DB ? initDB() : console.log("No Initalization");
+setInterval(getData, INTERVAL);
 
 
 /**
@@ -47,40 +47,45 @@ setInterval(getData,INTERVAL);
  * INITIALIZATION
  * ##########################################################################
  */
-function initDB()
-{
-	console.log("Starting Initialization")
-	mongo.connect(MONGO_URL, function(err, db) {
-		deleteDB(db, (err,result)=>{
-			if(err)
-				console.log("ERROR",error);
-			else
-			{
-				console.log("dropped Table");
-				createFields(db);
-			}
-		});
-	
-	});
+function initDB() {
+    console.log("Starting Initialization")
+    mongo.connect(MONGO_URL, function (err, db) {
+        deleteDB(db, (err, result) => {
+            if (err)
+                console.log("ERROR", error);
+            else {
+                console.log("dropped Table");
+                createFields(db);
+            }
+        });
+
+    });
 }
 /**
  * Creates the Fields for the Companies
  * @param db
  */
-function createFields(db)
-{
-	  symbols.forEach(symbol=>{
-			db.collection(COLLECTION_NAME).insertOne({symbol,values:[]});
-		});
-	
-	
-	console.log("Initialization finished");
-	console.log("_________________________________________________________")
+function createFields(db) {
+    symbols.forEach(symbol => {
+        let obj = {symbol, value: []};
+        getInformation(symbol)
+            .then(info => {
+                db.collection(COLLECTION_NAME).insertOne(Object.assign(JSON.parse(info.body),obj));
+            });
+    });
+
+
+    console.log("Initialization finished");
+    console.log("_________________________________________________________")
 }
-				  
-function deleteDB(db,callback)
-{
-	 db.collection(COLLECTION_NAME).deleteMany({},(err,result)=>callback(err,result));
+
+
+function getInformation(symbol) {
+    return requestify.get('https://api.iextrading.com/1.0/stock/' + symbol + '/company')
+}
+
+function deleteDB(db, callback) {
+    db.collection(COLLECTION_NAME).deleteMany({}, (err, result) => callback(err, result));
 }
 
 
@@ -94,7 +99,7 @@ function deleteDB(db,callback)
 /**
  * Fetches Data from the API
  */
-function getData(){
+function getData() {
     iteration++;
     console.log("Iteration " + iteration + " starting");
     fetchData(getURLs());
@@ -103,55 +108,51 @@ function getData(){
 /**
  * Generates the API URLs
  */
-function getURLs()
-{
-	let urls = [];
-	
-	symbols.forEach(company=>{
-		let url = standardURL.replace("%#company#%",company);
-		urls.push(url);
-	});
-	
-	return urls;
+function getURLs() {
+    let urls = [];
+
+    symbols.forEach(company => {
+        let url = standardURL.replace("%#company#%", company);
+        urls.push(url);
+    });
+
+    return urls;
 }
 
 /**
  * Fetches the information from the API
  * @param urls
  */
-function fetchData(urls)
-{	
-	
-	urls.forEach(url=>{
-		
-		requestify.get(url).then(response=>{
-				
-			saveInDb(response.getBody());
+function fetchData(urls) {
 
-		});
-	});
+    urls.forEach(url => {
+        requestify.get(url).then(response => {
+           if(response.getBody().latestPrice)
+            saveInDb(response.getBody());
+           else console.log("Exchange Closed");
+        });
+    });
 }
 
 /**
  * Pushes Information into the Database
  * @param body
  */
-function saveInDb(body)
-{
-	let date = moment().format('YYYY-MM-DD');
+function saveInDb(body) {
+    let date = moment().format('YYYY-MM-DD');
 
-	var value = {
-		"price":body.latestPrice,
-		"volume":body.latestVolume,
-		"change":body.change,
-		"time":body.latestTime,
-		"delayedPrice" : body.delayedPrice,
-		"delayedPriceTime" : body.delayedPriceTime,
-		date
-	}
-	
-	mongo.connect(MONGO_URL, function(err, db) {
-	   db.collection(COLLECTION_NAME).updateOne({symbol:body.symbol},{$push:{"values":value}})
-	   db.close();
-	});
+    var values = {
+        "price": body.latestPrice,
+        "volume": body.latestVolume,
+        "change": body.change,
+        "time": body.latestTime,
+        "delayedPrice": body.delayedPrice,
+        "delayedPriceTime": body.delayedPriceTime,
+        date
+    }
+
+    mongo.connect(MONGO_URL, function (err, db) {
+        db.collection(COLLECTION_NAME).updateOne({symbol: body.symbol}, {$push: {"values": values}})
+        db.close();
+    });
 }
