a32471de27486aaa600ef2376418ba77fa6a0b23
Markus <markus.boebel1@gmail.com>
2017-09-18 16:13:41 +0100

    Updated Dockerfile
    Managed Dockerization
    Added New Logging
    Adding some tests

---
+++ datamining/test/data.test.js
@@ -0,0 +1,31 @@
+/**
+ * Created by boebel on 18.09.2017.
+ */
+
+const chai = require('chai');
+const sinon = require('sinon');
+
+const data = require('../data');
+
+chai.should();
+
+
+describe("Datascript", function () {
+    describe("Database things", function () {
+        it("should drop Table if not exists", async function () {
+            let result = await data.dropTable();
+            result.status.should.equal('DROP TABLE');
+        });
+        it("should drop Table", async function () {
+
+            let result = await data.createDatabase();
+            result.status.should.equal('CREATE TABLE');
+        });
+    });
+    describe("help functions", function () {
+        it("should get urls", async function () {
+            let result = await data.getURLs();
+            result.should.be.a('array');
+        });
+    });
+});
