fcca918882885f5970a5f738432250ca7af80577
Markus <markus.boebel1@gmail.com>
2017-09-26 13:09:11 +0100

    Rebuild the UI
    Introduced new Logging for Server

---
+++ webapp/src/js/texts.js
@@ -0,0 +1,30 @@
+/**
+ * Created by boebel on 26.09.2017.
+ */
+
+
+ let texts = {
+
+  overview: {
+    node: {
+      picture: "img/nodejs.png",
+      caption: "Webserver",
+      title: "NodeJs",
+      description: "od tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua" + ".Atvero eos et accusam et jus"
+    },
+    mesosphere: {
+      picture: "img/mesos.png",
+      caption: "Container Management",
+      title: "NodeJs",
+      description: "od tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua" + ".Atvero eos et accusam et jus"
+    },
+    jMeter: {
+      picture: "img/apache.png",
+      caption: "Webserver",
+      title: "NodeJs",
+      description: "od tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua" + ".Atvero eos et accusam et jus"
+    }
+  }
+}
+
+module.exports = texts;
--- webapp/src/js/views/index.component/overview.component.js
+++ webapp/src/js/views/index.component/overview.component.js
@@ -8,93 +8,174 @@ import Box from 'grommet/components/Box';
 import Paragraph from 'grommet/components/Paragraph';
 import Image from 'grommet/components/Image';
 import Heading from 'grommet/components/Heading';
+import Table from 'grommet/components/Table';
+import TableRow from 'grommet/components/TableRow';
 
-import {Row, Col, Container} from "react-grid-system";
+import Texts from '../../texts';
+
+import {Row, Col, Container, Visible, Hidden} from "react-grid-system";
 
 
 export default class OverviewComponent extends Component {
 
   render() {
+
+    let texts = Texts.overview;
+
     return (
+
       <Box pad="large">
-      <Container fluid={true}>
-        <Row style={{"marginBottom":"3%"}}>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
-            <Image size="small" src="img/nodejs-new-pantone-black.png"/>
-          </Col>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
-            <Image size="small" src="img/mesos.png"/>
-          </Col>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
-            <Image size="small" src="img/apache.png" />
-          </Col>
-        </Row>
-        <Row>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
-            <Heading strong={false}
-                     uppercase={true}
-                     tag='h3'>
-              Webserver
-            </Heading>
-          </Col>
+        <Visible xl>
+          <Container fluid={true}>
+            <Row style={{"marginBottom": "3%"}}>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
+                <Image size="small" src={texts.node.picture}/>
+              </Col>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
+                <Image size="small" src={texts.mesosphere.picture}/>
+              </Col>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
+                <Image size="small" src={texts.jMeter.picture}/>
+              </Col>
+            </Row>
+            <Row>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h3'>
+                  {texts.node.caption}
+                </Heading>
+              </Col>
 
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
-            <Heading strong={false}
-                     uppercase={true}
-                     tag='h3'>
-              Container Management
-            </Heading>
-          </Col>
-          <Col xl={4} sm={2} xs={2} style={{"padding":"0 5% 0 5%"}}>
-            <Heading strong={false}
-                     uppercase={true}
-                     tag='h3'>
-              Traffic Simulation
-            </Heading>
-          </Col>
-          <Col xl={4} sm={2} xs={2} style={{"padding":"0 5% 0 5%"}}/>
-        </Row>
-        <Row>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
-            <Heading strong={false}
-                     uppercase={true}
-                     tag='h1'>
-              NodeJS
-            </Heading>
-          </Col>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
-            <Heading strong={false}
-                     uppercase={true}
-                     tag='h1'>
-              Mesosphere
-            </Heading>
-          </Col>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
-            <Heading strong={false}
-                     uppercase={true}
-                     tag='h1'>
-              JMeter
-            </Heading>
-          </Col>
-        </Row>
-        <Row>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
-            <Paragraph size='medium'>od tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-              vero eos et accusam et jus</Paragraph>
-          </Col>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
-              <Paragraph size='medium'>od tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus
-                est Lorem ipsum dolor</Paragraph>
-          </Col>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h3'>
+                  {texts.mesosphere.caption}
+                </Heading>
+              </Col>
+              <Col xl={4} sm={2} xs={2} style={{"padding": "0 5% 0 5%"}}>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h3'>
+                  {texts.jMeter.caption}
+                </Heading>
+              </Col>
+              <Col xl={4} sm={2} xs={2} style={{"padding": "0 5% 0 5%"}}/>
+            </Row>
+            <Row>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h1'>
+                  {texts.node.title}
+                </Heading>
+              </Col>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h1'>
+                  {texts.mesosphere.title}
+                </Heading>
+              </Col>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h1'>
+                  {texts.jMeter.title}
+                </Heading>
+              </Col>
+            </Row>
+            <Row>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
+                <Paragraph size='medium'>{texts.node.description}</Paragraph>
+              </Col>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
+                <Paragraph size='medium'>{texts.mesosphere.description}
+                </Paragraph>
+              </Col>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}>
 
-              <Paragraph size='medium'>od tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At
-                vero eos et accusam et justo duo </Paragraph>
-          </Col>
-          <Col xl={4} style={{"padding":"0 5% 0 5%"}}/>
-        </Row>
-      </Container>
+                <Paragraph size='medium'>{texts.jMeter.description} </Paragraph>
+              </Col>
+              <Col xl={4} style={{"padding": "0 5% 0 5%"}}/>
+            </Row>
+          </Container>
+        </Visible>
+        <Hidden xl>
+          <Table responsive={true}>
+            <tbody>
+            <TableRow>
+              <td>
+                <Image size="small" src={texts.node.picture}/>
+              </td>
+              <td>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h3'>
+                  {texts.node.caption}
+                </Heading>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h1'>
+                  {texts.jMeter.title}
+                </Heading>
+              </td>
+              <Hidden xs>
+                <td>
+
+                  {texts.node.description}
+                </td>
+              </Hidden>
+            </TableRow>
+            <TableRow>
+              <td>
+                <Image size="small" src={texts.mesosphere.picture}/>
+              </td>
+              <td>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h3'>
+                  {texts.mesosphere.caption}
+                </Heading>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h1'>
+                  {texts.mesosphere.title}
+                </Heading>
+              </td>
+              <Hidden xs>
+                <td>
+                  {texts.mesosphere.description}
+                </td>
+              </Hidden>
+            </TableRow>
+            <TableRow>
+              <td>
+                <Image size="small" src={texts.jMeter.picture}/>
+              </td>
+              <td>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h3'>
+                  {texts.jMeter.caption}
+                </Heading>
+                <Heading strong={false}
+                         uppercase={true}
+                         tag='h1'>
+                  {texts.jMeter.title}
+                </Heading>
+              </td>
+              <Hidden xs>
+                <td>
+                  {texts.jMeter.description}
+                </td>
+              </Hidden>
+            </TableRow>
+            </tbody>
+          </Table>
+
+        </Hidden>
       </Box>
     );
   }
